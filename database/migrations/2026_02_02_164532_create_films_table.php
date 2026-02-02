bf817a5a6f7542a1666c2cc07bd4fb0d11cb3e9a
aniol <aniolnicolauroig@gmail.com>
2026-02-02 18:09:55 +0100

    Making and executing the films SQL migration

--- database/migrations/2026_02_02_164532_create_films_table.php
+++ database/migrations/2026_02_02_164532_create_films_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('films', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->integer('year');
+            $table->string('genre', 50);
+            $table->string('country', 30);
+            $table->integer('duration');
+            $table->string('img_url', 255);
             $table->timestamps();
         });
     }
