6e3ff1211cb22ec9ee9e68973f5b33dc3c53cdd5
aniol <aniolnicolauroig@gmail.com>
2026-02-17 18:55:57 +0100

    little unpredicted fix

--- database/migrations/2026_02_17_175532_add_agency_data_field_in_actors_table.php
+++ database/migrations/2026_02_17_175532_add_agency_data_field_in_actors_table.php
@@ -6,17 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('actors', function (Blueprint $table) {
-            $table->string('agency')->nullable()->after('name');
+            //
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('actors', function (Blueprint $table) {
-            $table->dropColumn('agency');
+            //
         });
     }
 };
